36e3a21b25091364f29eaccdc908f666b2021816
Edvin Syse <es@syse.no>
2016-11-16 20:35:16 +0100

    Updated to TornadoFX 1.5.8, javafx-maven-plugin to 8.7.0, Kotlin 1.0.5 and added a main method for backwards compatibility (https://github.com/edvin/tornadofx-quickstart-archetype/issues/2)

--- src/main/resources/archetype-resources/src/main/kotlin/MyApp.kt
+++ src/main/resources/archetype-resources/src/main/kotlin/MyApp.kt
@@ -4,3 +4,10 @@ import ${package}.view.MainView
 import tornadofx.App
 
 class MyApp: App(MainView::class, Styles::class)
+
+/**
+ * The main method is needed to support the mvn jfx:run goal.
+ */
+fun main(args: Array<String>) {
+    Application.launch(MyApp::class.java, *args)
+}
